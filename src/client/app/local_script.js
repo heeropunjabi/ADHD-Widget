0d89cf45cd22b39ba3e60de9b0d0318918c6e2c7
Heero Punjabi <heeropunjabi@gmail.com>
2018-02-11 14:31:52 +0530

    https for all ajax calls . - heero

--- src/client/app/local_script.js
+++ src/client/app/local_script.js
@@ -3,7 +3,8 @@ $(function () {
         $("#test-progress").css("display","block");
     });
     $('#start-test-button').attr('disabled', 'disabled');
-    $.get("http://adhdassist.azurewebsites.net/Portal/GetSettings?settingName=focus%20hyperactivity%204", function (data, status) {
+    var protocol = document.location.protocol;
+    $.get((protocol+"//adhdassist.azurewebsites.net/Portal/GetSettings?settingName=focus%20hyperactivity%204"), function (data, status) {
         var settings = setSettings(data);
         $('#target-minutes-placeholder').html(settings.totalTime / (1000 * 60));
         $('#start-test-button').removeAttr('disabled').click(function () {
@@ -12,7 +13,7 @@ $(function () {
             application.createCountdownTracker('#countdown', settings.totalTime);
             startTest('#workspace-container', '#workspace-results', '#additional-controls', function (results, immediates, newRank) {
                 $.post(
-                    'http://adhdassist.azurewebsites.net/Report/PersistTestResults',
+                    (protocol+'//adhdassist.azurewebsites.net/Report/PersistTestResults'),
                     {
                         profileId: '6b1387e4-57ea-42b2-bd52-0af69631bfb6',
                         testParameters: JSON.stringify(data),
